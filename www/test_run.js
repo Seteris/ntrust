5aa566969b42a9aa3a2ca32c7026b6a4652715d9
Patrick Thorbauer <patrick.thorbauer@gmail.com>
2021-09-29 19:07:21 +0200

    chore(js): fix test counter, cleanup code

--- www/test_run.js
+++ www/test_run.js
@@ -1,12 +1,15 @@
 import * as wasm from "crypto_test";
 import {provideData} from "./test_data";
 
-let passed_test_count  = 0;
-let failed_test_count  = 0;
-let failed_tests = [];
+let passed_test_count = 0;
+let failed_test_count = 0;
+let failed_tests      = [];
 
 export function runTests() {
-    let pass = false;
+    let pass          = false;
+    passed_test_count = 0;
+    failed_test_count = 0;
+    failed_tests      = [];
     for (let i = 1; i <= 10; i++) {
         let chunk = provideData(i);
         for (let j = 0; j < chunk.length; j++) {
@@ -17,10 +20,6 @@ export function runTests() {
             }
         }
     }
-    // pass = runTest(comparison_pk, comparison_sk, seed);
-
-
-
     alert((passed_test_count + failed_test_count) + " tests run. " + passed_test_count + " passed. " + failed_test_count + " failed.");
     console.log("List of failed tests: " + failed_tests);
 }
